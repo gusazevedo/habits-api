3dcfbe02c61d7e818cfff413b7bf1de39a5f41f0
Gustavo Azevedo <gustavoazevedogan@gmail.com>
2023-01-22 11:35:05 -0300

    Rotas de detalhes dos dias com habitos

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,77 @@
 import {prisma} from "./lib/prisma";
 import {FastifyInstance} from "fastify";
+import {z} from 'zod';
+import dayjs from "dayjs";
 
 export async function appRoutes(app: FastifyInstance) {
-    app.get('/', async () => {
-        const habits = await prisma.habit.findMany();
+    app.post('/habits', async (request) => {
+        const createhabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(
+                z.number()
+                    .min(0)
+                    .max(6)
+            )
+        });
 
-        return habits;
+        const {title, weekDays} = createhabitBody.parse(request.body);
+        const today = dayjs().startOf('day').toDate();
+
+        await prisma.habit.create({
+            data: {
+                title,
+                created_at: today,
+                WeekDays: {
+                    create: weekDays.map(weekDay => {
+                        return {
+                            week_day: weekDay
+                        }
+                    })
+                }
+            }
+        })
+    });
+
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            // Converte o valor enviado pelo front
+            // de string para o objeto Date.
+            date: z.coerce.date()
+        });
+
+        const {date} = getDayParams.parse(request.query);
+        const parsedDate = dayjs(date).startOf('day');
+        const weekDay = parsedDate.get('day');
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date,
+                },
+                WeekDays: {
+                    some: {
+                        week_day: weekDay
+                    }
+                }
+            }
+        });
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate(),
+            },
+            include: {
+                DayHabit: true,
+            }
+        });
+
+        const completedHabits = day?.DayHabit.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+
+        return {
+            possibleHabits,
+            completedHabits
+        };
     });
 }
